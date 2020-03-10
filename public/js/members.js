a7e21c94cdc39516b0a7cf4421dab51d18ced115
drthisguy <page.c.tyler@gmail.com>
2020-03-10 16:40:58 -0400

    added datatable cdn package to manage the main expenses table.

--- public/js/members.js
+++ public/js/members.js
@@ -1,6 +1,14 @@
 $(document).ready(function() {
 
+
+  //init datatables
+  $("#table").DataTable({
+    responsive: true
+  });
+
+  //init materialize 
   M.AutoInit();
+
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
   $.get("/api/user_data").then(function(data) {
