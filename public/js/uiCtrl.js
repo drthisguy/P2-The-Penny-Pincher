6b67917a74342e76717720438a0f22eccbbd4dde
drthisguy <page.c.tyler@gmail.com>
2020-03-12 02:29:02 -0400

    buttons for the edit state have been added to the controller card.

---
+++ public/js/uiCtrl.js
@@ -0,0 +1,52 @@
+
+
+export class UICtrl  {
+
+    constructor() { 
+      this.name=  $("#name-field").val().trim(),
+      this.amount= $("#amount-field").val().trim(),
+      this.category= $("#category-field").val().trim(),
+      this.priority= $("#priority-field").val().trim()
+      this.addBtn = $('#add');
+      this.addBtn = $('#add');
+      this.updateBtn = $('#update-btn');
+      this.deleteBtn = $('#delete-btn');
+      this.cancelBtn = $('#cancel-btn');
+      
+    }
+
+
+    addNewItem(user) {
+        const user = await ItemCtrl.getUser(),
+
+        newItem = {
+          user_id: user.id,
+          name:  this.name,
+          amount: this.amount.replace(/[$,]/gi, ""),  //remove $ sign and commas,
+          category: this.category,
+          priority: this.priority
+        }
+        ItemCtrl.newPost(newItem).then( response => {
+        console.log("UICtrl -> addNewItem -> response", response)
+         
+        }).catch(err => {
+          if (err.status === 501) {
+           console.log('Invalid Input. Please try again');
+          } 
+        })
+    }
+
+    startEditState() {
+        this.updateBtn.show();
+        this.cancelBtn.show();
+        this.deleteBtn.show();
+        this.addBtn.hide();
+    }
+
+    stopEditState() {
+        this.updateBtn.hide();
+        this.cancelBtn.hide();
+        this.deleteBtn.hide();
+        this.addBtn.show();
+    }
+}
